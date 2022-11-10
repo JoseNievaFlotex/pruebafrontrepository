d0b859f37c35247d5c84bb637a94ba359bf1a1fa
joseNievaFlotex <jose.nieva@flotex.pe>
2022-11-10 10:02:01 -0500

    integrando responsive

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -7,19 +7,23 @@ import React from "react";
 const About = () => {
   return (
     <>
-      <Row className="exposicion">
-    
-        <video 
-          src="https:\/\/adar.com.br\/wp-content\/uploads\/2020\/01\/Grupo-Adar_2.mp4"
-          autoPlay={true}
-          muted={true}
-          loop={true}
-          poster="/img/banner-1.png"
-        ></video>
-
-        <Container className="col-sm-8 p-5">
-          <h1 className="m-5 text-center display-1"> FLOTEX</h1>
-          <span className=" m-3 text-center">
+      <Container>
+        <Row >
+        <iframe className="elementor"
+            frameborder="0"
+            allowfullscreen="1"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            title="SLIDER CLADD NUEVO"
+            src="https://www.youtube.com/embed/JXDahVfZY6U?autoplay=1&amp;mute=1&amp;loop=1&amp;controls=0&amp;rel=0&amp;playsinline=1&amp;enablejsapi=1&amp;origin=https%3A%2F%2Fwww.cladd.com.ar&amp;widgetid=1"
+            id="widget2"
+            
+          ></iframe>
+        </Row>
+      </Container>
+      <Container className="text-center">
+        <Row >
+          <h1 > FLOTEX</h1>
+          <h5  >
             O Grupo, composto pelas empresas Adar, Artec e Texliving, atua em
             diferentes segmentos do mercado têxtil sendo hoje um dos maiores e
             ma is completos do Brasil. Cada marca possui as características
@@ -29,61 +33,61 @@ const About = () => {
             decoração. Uma equipe treinada de representantes atende clientes em
             todo território nacional, oferecendo o melhor mix de p rodutos para
             cada região.
-          </span>
-        </Container>
-        <Container className="d-flex justify-content-center ">
-          <Row className="p-2">
-            <iframe class="elementor-background-video-embed" frameborder="0" allowfullscreen="1"
-             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-             title="SLIDER CLADD NUEVO"  
-             src="https://www.youtube.com/embed/9dU8k63hboI?autoplay=1&amp;mute=1&amp;loop=1&amp;controls=0&amp;rel=0&amp;playsinline=1&amp;enablejsapi=1&amp;origin=https%3A%2F%2Fwww.cladd.com.ar&amp;widgetid=1"
-             id="widget2"  
-             style={{ width: "1000px", height: "549px" }}
-             ></iframe>
-
-          </Row>
-        </Container>
-        {""}
-        <Container className=" bg-dark text-light fluid col-sm-10">
-          <h3 className="m-5 text-center "> Nuestros Valores</h3>
-          <Row className="banner">
-            <Col>
-              <Image
-                className="logoempresa"
-                src="https://paistextil.com/wp-content/uploads/2020/09/Grupo-41-1.svg"
-              />
-              <h5 className="text-center p-3">
-                {" "}
-                Creemos en la calidad que se logra en producciones textiles.
-              </h5>
-            </Col>
-            <Col>
-              <Image
-                className="logoempresa"
-                src="https://paistextil.com/wp-content/uploads/2020/09/Grupo-37.svg"
-              />
-              <h5 className="text-center p-3">
-                {" "}
-                Capacitamos a nuestro equipo y 
-                ayudamos a mejorar la calidad de la 
-                atención para que tengan mayor valor comercial{" "}
-              </h5>
-            </Col>
-            <Col>
-              <Image
-                className="logoempresa"
-                src="https://paistextil.com/wp-content/uploads/2020/09/Grupo-42.svg"
-              />
-              <h5 className="text-center p-3">
-                {" "}
-                Empoderamos mujeres. Más del 
-                50% de nuestro equipo está conformado por mujeres.
-              </h5>
-            </Col>
-          </Row>
-        </Container>
-      </Row>
-      <Row className="banner"></Row>
+          </h5>
+        </Row>
+      </Container>
+      <Container className="d-flex justify-content-center ">
+        <Row className="p-2">
+          <iframe
+            class="elementor-background-video-embed"
+            frameborder="0"
+            allowfullscreen="1"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            title="SLIDER CLADD NUEVO"
+            src="https://www.youtube.com/embed/9dU8k63hboI?autoplay=1&amp;mute=1&amp;loop=1&amp;controls=0&amp;rel=0&amp;playsinline=1&amp;enablejsapi=1&amp;origin=https%3A%2F%2Fwww.cladd.com.ar&amp;widgetid=1"
+            id="widget2"
+            style={{ width: "1000px", height: "549px" }}
+          ></iframe>
+        </Row>
+      </Container>
+      {""}
+      <Container className=" bg-dark text-light fluid col-sm-6 col-md-8 col-md-10">
+        <h3 className="m-5 text-center "> Nuestros Valores</h3>
+        <Row className="banner">
+          <Col>
+            <Image
+              className="logoempresa"
+              src="https://paistextil.com/wp-content/uploads/2020/09/Grupo-41-1.svg"
+            />
+            <h5 className="text-center p-3">
+              {" "}
+              Creemos en la calidad que se logra en producciones textiles.
+            </h5>
+          </Col>
+          <Col>
+            <Image
+              className="logoempresa"
+              src="https://paistextil.com/wp-content/uploads/2020/09/Grupo-37.svg"
+            />
+            <h5 className="text-center p-3">
+              {" "}
+              Capacitamos a nuestro equipo y ayudamos a mejorar la calidad de la
+              atención para que tengan mayor valor comercial{" "}
+            </h5>
+          </Col>
+          <Col>
+            <Image
+              className="logoempresa"
+              src="https://paistextil.com/wp-content/uploads/2020/09/Grupo-42.svg"
+            />
+            <h5 className="text-center p-3">
+              {" "}
+              Empoderamos mujeres. Más del 50% de nuestro equipo está conformado
+              por mujeres.
+            </h5>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
