7a2430312c7fab28487a26d31ddd10714ea2ca3c
joseNievaFlotex <jose.nieva@flotex.pe>
2022-11-11 09:55:40 -0500

    ordenamiento de carpetas de imagenes y agregando mas datos en home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -28,42 +28,55 @@ const Home = () => {
     </Carousel><Container className="p-4 d-flex justify-content-center animate__animated animate__backInLeft animate__delay demo-top-flotex">
         <Row className="col-sm-8">
           <h3 className="letter">LÍNEA SOSTENIBLE</h3>
-          <Image src="/img/flotex-green.jpg" />
+          <Image src="/img/flotexgreen/flotex-green.jpg" />
         </Row>
       </Container><Container className="d-flex justify-content-center p-2 ">
         <Row className="col-12 justify-content-center animate__animated animate__backInUp animate__delay demo-top-flotex">
           <div className="col-sm-8 row">
-            <Image src="/img/VP1.jpg" className=" img-responsive-unoverso" />
+            <Image src="/img/flotexgreen/VP1.jpg" className=" img-responsive-unoverso" />
           </div>
           <Col className="col-sm-2">
             <h3 className="tejido"> TU UNIVERSO EN TELAS</h3>
           </Col>
         </Row>
-      </Container><h2 className="text-center m-4">PROMOCIONES</h2><Container className="galleria col-sm-8 col-md-8 col-lg-8">
+
+      </Container>
+      <Container className="d-flex justify-content-center">
+        <Row className="col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+          <Image  src="/img/prueba/pruebacottom.png"  className="pruebadeface"/>
+        </Row>
+        
+
+      </Container>
+    
+      <h2 className="text-center m-4">PROMOCIONES</h2>
+
+      <Container className="galleria col-sm-8 col-md-8 col-lg-8">
+        
         <Row className="col-sm-4 p-1">
-          <Image src="/img/Ribest2.png" className="efecto" />
+          <Image src="/img/telas/Ribest2.png" className="efecto" />
         </Row>
         <Row className="col-sm-4 p-1">
-          <Image src="/img/cataniace1.png" className="efecto" />
+          <Image src="/img/telas/cataniace1.png" className="efecto" />
         </Row>
         <Row className="col-sm-4 p-1">
-          <Image src="/img/linoest1.png" className="efecto" />
+          <Image src="/img/telas/linoest1.png" className="efecto" />
         </Row>
         <Row className="col-sm-4 p-1">
-          <Image src="/img/chalisest2.png" className="efecto" />
+          <Image src="/img/telas/chalisest2.png" className="efecto" />
         </Row>
       </Container><Container className="galleria col-sm-8 col-md-8 col-lg-8">
         <Row className="col-sm-4 p-1">
-          <Image src="/img/chambrayest1.png" className="efecto" />
+          <Image src="/img/telas/chambrayest1.png" className="efecto" />
         </Row>
         <Row className="col-sm-4 p-1">
-          <Image src="/img/interlock1.png" className="efecto" />
+          <Image src="/img/telas/interlock1.png" className="efecto" />
         </Row>
         <Row className="col-sm-4 p-1">
-          <Image src="/img/loma2.png" className="efecto" />
+          <Image src="/img/telas/loma2.png" className="efecto" />
         </Row>
         <Row className="col-sm-4 p-1">
-          <Image src="/img/chambrayest2.png" className="efecto" />
+          <Image src="/img/telas/chambrayest2.png" className="efecto" />
         </Row>
       </Container></>
   );
